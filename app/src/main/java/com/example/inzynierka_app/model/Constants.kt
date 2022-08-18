36f5efca33550abed05fe71f25e2ca222c325176
Anna-Surma <anna.surma2000@gmail.com>
2022-08-18 19:32:03 +0200

    Add Siemens CA certificate

--- app/src/main/java/com/example/inzynierka_app/model/Constants.kt
+++ app/src/main/java/com/example/inzynierka_app/model/Constants.kt
@@ -1,8 +1,6 @@
 package com.example.inzynierka_app.model
 
 object Constants{
-    const val BASE_URL = "http://192.168.0.1"
-    //const val BASE_URL = "https://patronage22-poznan-js-seven.vercel.app/"
+    const val BASE_URL = "https://192.168.8.50"
     const val LOGIN_URL = "/api/jsonrpc"
-    // const val LOGIN_URL = "api/users/signin"
 }
