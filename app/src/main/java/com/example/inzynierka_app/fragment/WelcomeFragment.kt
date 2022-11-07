4867f7a2c51f33ab5817e9225309629fb92b09c7
Anna <anna.surma2000@gmail.com>
2022-11-07 13:13:48 +0100

    Change findViewById on view Binding in Welcome Fragment

--- app/src/main/java/com/example/inzynierka_app/fragment/WelcomeFragment.kt
+++ app/src/main/java/com/example/inzynierka_app/fragment/WelcomeFragment.kt
@@ -5,23 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.findNavController
 import com.example.inzynierka_app.R
+import com.example.inzynierka_app.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private  var _binding: FragmentWelcomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
-        val startButton = view.findViewById<Button>(R.id.btnStartButton)
+    ): View {
 
-        startButton.setOnClickListener {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.btnStartButton.setOnClickListener {
             view.findNavController()
                 .navigate(R.id.action_welcomeFragment_to_loginFragment)
         }
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
