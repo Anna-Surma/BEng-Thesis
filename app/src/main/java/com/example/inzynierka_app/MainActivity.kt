6b9311a2ffd7cf533ade1673c097d0e94094a416
Anna <anna.surma2000@gmail.com>
2022-11-07 13:12:45 +0100

    Add navigation to error fragment after clicking on error icon. Add room gradle

--- app/src/main/java/com/example/inzynierka_app/MainActivity.kt
+++ app/src/main/java/com/example/inzynierka_app/MainActivity.kt
@@ -13,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var errorActive = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +28,17 @@ class MainActivity : AppCompatActivity() {
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNav)
         bottomNavView.setupWithNavController(navController)
 
+        binding.ivErrorIcon.setOnClickListener {
+            if (!errorActive) {
+                binding.tvMode.visibility = View.GONE
+                binding.fcvMode.visibility = View.GONE
+                binding.navHostFragment.visibility = View.GONE
+                binding.bottomNav.visibility = View.GONE
+                binding.fcvError.visibility = View.VISIBLE
+                errorActive = true
+            }
+        }
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.autoFragment -> {
@@ -34,25 +46,44 @@ class MainActivity : AppCompatActivity() {
                     binding.fcvMode.visibility = View.VISIBLE
                     binding.tvMode.visibility = View.VISIBLE
                     binding.tvMode.setText(R.string.auto_mode)
+                    binding.ivErrorIcon.visibility = View.VISIBLE
+                    binding.fcvError.visibility = View.GONE
                 }
                 R.id.manualFragment -> {
                     binding.bottomNav.visibility = View.VISIBLE
                     binding.fcvMode.visibility = View.VISIBLE
                     binding.tvMode.visibility = View.VISIBLE
                     binding.tvMode.setText(R.string.manual_mode)
+                    binding.ivErrorIcon.visibility = View.VISIBLE
+                    binding.fcvError.visibility = View.GONE
                 }
                 R.id.blockFragment -> {
                     binding.bottomNav.visibility = View.VISIBLE
                     binding.fcvMode.visibility = View.VISIBLE
                     binding.tvMode.visibility = View.VISIBLE
                     binding.tvMode.setText(R.string.block_mode)
+                    binding.ivErrorIcon.visibility = View.VISIBLE
+                    binding.fcvError.visibility = View.GONE
                 }
                 else -> {
                     binding.bottomNav.visibility = View.GONE
                     binding.fcvMode.visibility = View.GONE
                     binding.tvMode.visibility = View.GONE
+                    binding.ivErrorIcon.visibility = View.GONE
+                    binding.fcvError.visibility = View.GONE
                 }
             }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (errorActive) {
+            binding.tvMode.visibility = View.VISIBLE
+            binding.fcvMode.visibility = View.VISIBLE
+            binding.navHostFragment.visibility = View.VISIBLE
+            binding.bottomNav.visibility = View.VISIBLE
+            binding.fcvError.visibility = View.GONE
+            errorActive = false
         }
     }
 }
---
+++ app/src/main/java/com/example/inzynierka_app/fragment/ErrorFragment.kt
@@ -0,0 +1,31 @@
+package com.example.inzynierka_app.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.inzynierka_app.R
+import com.example.inzynierka_app.databinding.FragmentErrorBinding
+import com.example.inzynierka_app.databinding.FragmentSplashBinding
+
+class ErrorFragment : Fragment() {
+
+    private  var _binding: FragmentErrorBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentErrorBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
